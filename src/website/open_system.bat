5a598c17f987b869312afe2fef84cb485535e9ca
haynako0 <hayspanibagongaltnanaman@gmail.com>
2025-05-31 23:58:27 +0800

    Provided additional instructions for system operations

--- src/website/open_system.bat
+++ src/website/open_system.bat
@@ -1,3 +1,4 @@
 @echo off
 start "" "http://localhost/lagunapho/"
 start "" "wscript.exe" "C:\Users\erlte\chatbotproject\nocons.vbs"
+REM make sure to change the file paths respectively (same goes for the chatbot files)
